8228b1842925055963d46c2d3e3f06ce2315e326
melpederneraa <167656054+melpederneraa@users.noreply.github.com>
2024-10-22 19:42:37 -0300

    Update Propietario.java

--- Propietario.java
+++ Propietario.java
@@ -1,22 +1,15 @@
 package org.example.Modelos;
 
-public class Propietario extends Persona{
+public class Propietario extends Persona {
+   public Propietario() {
+   }
 
+   public Propietario(int codigo, String nombre, String telefono) {
+      super(codigo, nombre, telefono);
+   }
 
-    public Propietario() {
-    }
-
-    public Propietario(int codigo, String nombre, String telefono) {
-        super(codigo, nombre, telefono);
-    }
-
-
-    @Override
-    public String toString() {
-        return "Propietario{" +
-                "codigo=" + this.getCodigo() +
-                ", nombre='" + this.getNombre() + '\'' +
-                ", telefono='" + this.getTelefono() + '\'' +
-                '}';
-    }
+   public String toString() {
+      int var10000 = this.getCodigo();
+      return "Propietario{codigo=" + var10000 + ", nombre='" + this.getNombre() + "', telefono='" + this.getTelefono() + "'}";
+   }
 }
