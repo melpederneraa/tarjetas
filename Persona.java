015a343f3324c4ac3a97092c15fd5f8f0f23c03a
melpederneraa <167656054+melpederneraa@users.noreply.github.com>
2024-10-22 19:41:52 -0300

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -1,49 +1,44 @@
 package org.example.Modelos;
 
 public class Persona {
-    private int codigo;
-    private String nombre;
-    private String telefono;
+   private int codigo;
+   private String nombre;
+   private String telefono;
 
-    public Persona() {
-    }
+   public Persona() {
+   }
 
-    public Persona(int codigo, String nombre, String telefono) {
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.telefono = telefono;
-    }
+   public Persona(int codigo, String nombre, String telefono) {
+      this.codigo = codigo;
+      this.nombre = nombre;
+      this.telefono = telefono;
+   }
 
-    public int getCodigo() {
-        return codigo;
-    }
+   public int getCodigo() {
+      return this.codigo;
+   }
 
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
+   public void setCodigo(int codigo) {
+      this.codigo = codigo;
+   }
 
-    public String getNombre() {
-        return nombre;
-    }
+   public String getNombre() {
+      return this.nombre;
+   }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+   public void setNombre(String nombre) {
+      this.nombre = nombre;
+   }
 
-    public String getTelefono() {
-        return telefono;
-    }
+   public String getTelefono() {
+      return this.telefono;
+   }
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
+   public void setTelefono(String telefono) {
+      this.telefono = telefono;
+   }
 
-    @Override
-    public String toString() {
-        return "Persona{" +
-                "codigo=" + codigo +
-                ", nombre='" + nombre + '\'' +
-                ", telefono='" + telefono + '\'' +
-                '}';
-    }
+   public String toString() {
+      return "Persona{codigo=" + this.codigo + ", nombre='" + this.nombre + "', telefono='" + this.telefono + "'}";
+   }
 }
