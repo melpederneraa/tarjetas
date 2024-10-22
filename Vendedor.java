e04ca8382530391c88a0ffced5819e0350ed695b
melpederneraa <167656054+melpederneraa@users.noreply.github.com>
2024-10-22 19:44:09 -0300

    Update Vendedor.java

--- Vendedor.java
+++ Vendedor.java
@@ -1,13 +1,14 @@
 package org.example.Modelos;
 
-public class Vendedor extends Persona{
-    private int legajo;
-    public Vendedor() {
-    }
+public class Vendedor extends Persona {
+   private int legajo;
 
-    public Vendedor(int legajo,int codigo, String nombre, String telefono) {
-        super(codigo, nombre, telefono);
-        this.legajo=legajo;
-    }
+   public Vendedor() {
+   }
 
+   public Vendedor(int legajo, int codigo, String nombre, String telefono) {
+      super(codigo, nombre, telefono);
+      this.legajo = legajo;
+   }
 }
+
