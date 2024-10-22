ab8cd7c7414bcbdbda1a0f11da53b37de464c7b7
melpederneraa <167656054+melpederneraa@users.noreply.github.com>
2024-10-22 19:39:13 -0300

    Update GestorPropietario.java

--- GestorPropietario.java
+++ GestorPropietario.java
@@ -1,54 +1,74 @@
 package org.example.Gestores;
 
-import org.example.Modelos.Propietario;
-import org.example.Modelos.Tarjeta;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import org.example.Modelos.Propietario;
 
 public class GestorPropietario {
-    private ArrayList<Propietario> propietarios = new ArrayList<>();
+   private ArrayList<Propietario> propietarios = new ArrayList();
 
-    public ArrayList<Propietario> getPropietarios() {
-        return propietarios;
-    }
+   public GestorPropietario() {
+   }
 
-    public void setPropietarios(ArrayList<Propietario> propietarios) {
-        this.propietarios = propietarios;
-    }
+   public ArrayList<Propietario> getPropietarios() {
+      return this.propietarios;
+   }
 
-    public void cargarPropietariosDesdeArchivo(){
-        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Usuario\\Documents\\UTN-FRVM\\Paradigma\\CodigoC2\\TarjetaGamer\\src\\main\\java\\org\\example\\propietarios.txt"))) {
+   public void setPropietarios(ArrayList<Propietario> propietarios) {
+      this.propietarios = propietarios;
+   }
+
+   public void cargarPropietariosDesdeArchivo() {
+      try {
+         BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\milag\\OneDrive\\Desktop\\example\\propietarios.txt"));
+
+         try {
+            br.readLine();
+
             String linea;
-            br.readLine();//linea de titulos, si la tengo, de esta manera la leo pero no la utilizo
-            while ((linea = br.readLine()) != null) {//mientras haya una linea mas en el archivo
-                String[] datos = linea.split(","); //tomo los datos
-
-                if (datos.length == 3) {
-                    String numero = datos[0].trim();
-                    String titular = datos[1].trim();
-                    String telefono = datos[2].trim();
-
-                    Propietario prop=new Propietario(Integer.parseInt(numero),titular,telefono);
-                    propietarios.add(prop);
-                }
+            while((linea = br.readLine()) != null) {
+               String[] datos = linea.split(",");
+               if (datos.length == 3) {
+                  String numero = datos[0].trim();
+                  String titular = datos[1].trim();
+                  String telefono = datos[2].trim();
+                  Propietario prop = new Propietario(Integer.parseInt(numero), titular, telefono);
+                  this.propietarios.add(prop);
+               }
             }
-        } catch (IOException e) {
-            System.out.println("Error al leer el archivo: " + e.getMessage());
-        }
-
-        System.out.println(propietarios);
-    }
-
-    public Propietario buscarPropietario(int codigo){
-        for (Propietario p: propietarios){
-            if(p.getCodigo()==codigo){
-                return p;
+         } catch (Throwable var9) {
+            try {
+               br.close();
+            } catch (Throwable var8) {
+               var9.addSuppressed(var8);
             }
-        }
-        return null;
-    }
 
+            throw var9;
+         }
+
+         br.close();
+      } catch (IOException var10) {
+         System.out.println("Error al leer el archivo: " + var10.getMessage());
+      }
+
+      System.out.println(this.propietarios);
+   }
+
+   public Propietario buscarPropietario(int codigo) {
+      Iterator var2 = this.propietarios.iterator();
+
+      Propietario p;
+      do {
+         if (!var2.hasNext()) {
+            return null;
+         }
+
+         p = (Propietario)var2.next();
+      } while(p.getCodigo() != codigo);
+
+      return p;
+   }
 }
