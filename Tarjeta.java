b4d163def61aeda8776e9e15781c3e6e35ecfd59
melpederneraa <167656054+melpederneraa@users.noreply.github.com>
2024-10-22 19:43:27 -0300

    Update Tarjeta.java

--- Tarjeta.java
+++ Tarjeta.java
@@ -1,44 +1,46 @@
 package org.example.Modelos;
 
 public class Tarjeta {
-    private int codigo;
-    private Propietario propietario;
-    private double saldo;
+   private int codigo;
+   private Propietario propietario;
+   private double saldo;
+   private Vendedor vendedor;
 
-    public Tarjeta() {
-    }
+   public Tarjeta() {
+   }
 
-    public Tarjeta(int codigo, Propietario propietario, double saldo) {
-        this.codigo = codigo;
-        this.propietario = propietario;
-        this.saldo = saldo;
-    }
+   public Tarjeta(int codigo, Propietario propietario, double saldo) {
+      this.codigo = codigo;
+      this.propietario = propietario;
+      this.saldo = saldo;
+   }
 
-    public int getCodigo() {
-        return codigo;
-    }
+   public int getCodigo() {
+      return this.codigo;
+   }
 
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
+   public void setCodigo(int codigo) {
+      this.codigo = codigo;
+   }
 
+   public Propietario getPropietario() {
+      return this.propietario;
+   }
 
-    public Propietario getPropietario() {
-        return propietario;
-    }
+   public void setPropietario(Propietario propietario) {
+      this.propietario = propietario;
+   }
 
-    public void setPropietario(Propietario propietario) {
-        this.propietario = propietario;
-    }
+   public double getSaldo() {
+      return this.saldo;
+   }
 
-    public double getSaldo() {
-        return saldo;
-    }
+   public void setSaldo(double saldo) {
+      this.saldo = saldo;
+   }
 
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-    public String toString() {
-        return  "Tarjeta: "+this.propietario+" Saldo: "+this.saldo;
-    }
+   public String toString() {
+      String var10000 = String.valueOf(this.propietario);
+      return "Tarjeta: " + var10000 + " Saldo: " + this.saldo;
+   }
 }
